4e8f85deb0c47608b7350dd83130f616f8e78f01
Ada Aster <an.ada.poirier@gmail.com>
2026-05-10 10:27:25 -0400

    Fix render (sound and particle borken)

---
+++ common/src/main/java/net/countered/terrainslabs/mixin/ontop/render/MixinBlockModelShaper.java
@@ -0,0 +1,26 @@
+package net.countered.terrainslabs.mixin.ontop.render;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.countered.terrainslabs.block.interfaces.IOffsetState;
+import net.minecraft.client.renderer.block.BlockModelShaper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.Map;
+
+@Mixin( BlockModelShaper.class )
+public abstract class MixinBlockModelShaper {
+
+    @WrapOperation( method = "getBlockModel", at = @At( value = "INVOKE", target = "Ljava/util/Map;get(Ljava/lang/Object;)Ljava/lang/Object;" ) )
+    private Object terrain_slabs$replaceCopiedState(
+            Map instance, Object o, Operation<Object> original
+    ) {
+        if ( !((IOffsetState) o).terrain_slabs$getOffset() ) {
+            return original.call( instance, o );
+        }
+
+        return original.call( instance, ((IOffsetState) o).terrain_slabs$getOppositeState() );
+    }
+
+}
