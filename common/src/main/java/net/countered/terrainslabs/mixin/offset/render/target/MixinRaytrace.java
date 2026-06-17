467d32bc7082a77dd9fa66a42666f22b1ed4bae0
Ada Aster <an.ada.poirier@gmail.com>
2026-06-17 07:54:23 -0400

    Fix Raytrace for Forge

--- common/src/main/java/net/countered/terrainslabs/mixin/offset/render/target/MixinRaytrace.java
+++ common/src/main/java/net/countered/terrainslabs/mixin/offset/render/target/MixinRaytrace.java
@@ -10,7 +10,7 @@ import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.VoxelShape;
-import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
@@ -20,26 +20,27 @@ import oshi.util.tuples.Pair;
 import java.util.ArrayList;
 import java.util.List;
 
-@Mixin( BlockGetter.class )
+@Mixin(BlockGetter.class)
 public interface MixinRaytrace {
 
+    // SPECIAL NOTE: Forge will not accept mixin overwrite if @Unique static methods are used.
+
     /**
-     * Raytrace fix for on top blocks
-     *
-     * @author Countered, Ada Aster
-     * @reason Architectury forge doesn't like lambda mixins :(
+     * Raytrace fix for on offset blocks
+     * @author Countered
+     * @author Ada Aster
+     * @reason Forge doesn't like lambda mixins :(
      */
     @Overwrite
     default BlockHitResult clip(ClipContext context) {
         return BlockGetter.traverseBlocks(
                 context.getFrom(), context.getTo(), context,
-                (clipContext, pos) -> {
-                    BlockGetter level = (BlockGetter) (Object) this;
+                (clipContext, blockPos) -> {
                     List<Pair<BlockHitResult, Double>> hits = new ArrayList<>();
-                    terrain_slabs$vanillaClip( level, clipContext, pos, hits );
+                    terrain_slabs$vanillaClip( clipContext, blockPos, hits );
 
-                    terrain_slabs$offsetClip( level, clipContext, pos.above(), hits );
-                    terrain_slabs$offsetClip( level, clipContext, pos.below(), hits );
+                    terrain_slabs$offsetClip( clipContext, blockPos, Direction.UP, hits );
+                    terrain_slabs$offsetClip( clipContext, blockPos, Direction.DOWN, hits );
 
                     hits.sort( (o1, o2) -> {
                         double diff = o1.getB() - o2.getB();
@@ -48,30 +49,56 @@ public interface MixinRaytrace {
 
                     return hits.get( 0 ).getA();
                 },
-                MixinRaytrace::terrain_slabs$vanillaMiss
+                this::terrain_slabs$vanillaMiss
         );
     }
 
     @Unique
-    private static void terrain_slabs$offsetClip(
-            BlockGetter level, ClipContext clipContext, BlockPos pos, List<Pair<BlockHitResult, Double>> list
+    default void terrain_slabs$offsetClip(
+            ClipContext clipContext, BlockPos pos, Direction dir, List<Pair<BlockHitResult, Double>> list
     ) {
-        BlockState state = level.getBlockState(pos);
-        if ( !((IOffsetState) state).terrain_slabs$hasOffsetState() ) {
+        pos = pos.relative( dir );
+        BlockState state = this.getBlockState( pos );
+        if ( dir == Direction.UP && !((IOffsetState) state).terrain_slabs$isOffsetAbove() ) {
+            return;
+        } else if ( dir == Direction.DOWN && !((IOffsetState) state).terrain_slabs$isOffsetBelow() ) {
             return;
         }
 
         Vec3 from = clipContext.getFrom();
         Vec3 to = clipContext.getTo();
-        VoxelShape voxelShape3 = clipContext.getBlockShape(state, level, pos);
-        BlockHitResult blockHitResult = level.clipWithInteractionOverride(from, to, pos, voxelShape3, state);
-        double dist = blockHitResult == null ? Double.MAX_VALUE : from.distanceToSqr(blockHitResult.getLocation());
+        VoxelShape voxelShape3 = clipContext.getBlockShape(state, (BlockGetter) this, pos);
+        BlockHitResult blockHitResult = this.clipWithInteractionOverride(from, to, pos, voxelShape3, state);
+        if ( blockHitResult == null ) {
+            return;
+        }
 
+        double dist = from.distanceToSqr(blockHitResult.getLocation());
         list.add( new Pair<>( blockHitResult, dist ) );
     }
 
     @Unique
-    private static BlockHitResult terrain_slabs$vanillaMiss( ClipContext ctx ) {
+    default void terrain_slabs$vanillaClip(
+            ClipContext clipContext, BlockPos pos, List<Pair<BlockHitResult, Double>> list
+    ) {
+        BlockState blockState = this.getBlockState(pos);
+        FluidState fluidState = this.getFluidState(pos);
+        Vec3 from = clipContext.getFrom();
+        Vec3 to = clipContext.getTo();
+
+        VoxelShape voxelShape = clipContext.getBlockShape(blockState, (BlockGetter) this, pos);
+        BlockHitResult blockHitResult = this.clipWithInteractionOverride(from, to, pos, voxelShape, blockState);
+        double dist1 = blockHitResult  == null ? Double.MAX_VALUE : from.distanceToSqr(blockHitResult.getLocation());
+        list.add( new Pair<>( blockHitResult, dist1 ) );
+
+        VoxelShape voxelShape2 = clipContext.getFluidShape(fluidState, (BlockGetter) this, pos);
+        BlockHitResult blockHitResult2 = voxelShape2.clip(from, to, pos);
+        double dist2 = blockHitResult2 == null ? Double.MAX_VALUE : from.distanceToSqr(blockHitResult2.getLocation());
+        list.add( new Pair<>( blockHitResult2, dist2 ) );
+    }
+
+    @Unique
+    default BlockHitResult terrain_slabs$vanillaMiss( ClipContext ctx ) {
         Vec3 vec3 = ctx.getFrom().subtract(ctx.getTo());
         return BlockHitResult.miss(
                 ctx.getTo(),
@@ -80,27 +107,13 @@ public interface MixinRaytrace {
         );
     }
 
-    @Unique
-    private static void terrain_slabs$vanillaClip(
-            BlockGetter level, ClipContext clipContext, BlockPos pos, List<Pair<BlockHitResult, Double>> list
-    ) {
-        BlockState blockState = level.getBlockState(pos);
-        FluidState fluidState = level.getFluidState(pos);
-        Vec3 from = clipContext.getFrom();
-        Vec3 to = clipContext.getTo();
-
-        VoxelShape voxelShape = clipContext.getBlockShape(blockState, level, pos);
-        BlockHitResult blockHitResult = level.clipWithInteractionOverride(from, to, pos, voxelShape, blockState);
-        double dist1 = blockHitResult  == null ? Double.MAX_VALUE : from.distanceToSqr(blockHitResult.getLocation());
-        list.add( new Pair<>( blockHitResult, dist1 ) );
-
-        VoxelShape voxelShape2 = clipContext.getFluidShape(fluidState, level, pos);
-        BlockHitResult blockHitResult2 = voxelShape2.clip(from, to, pos);
-        double dist2 = blockHitResult2 == null ? Double.MAX_VALUE : from.distanceToSqr(blockHitResult2.getLocation());
-        list.add( new Pair<>( blockHitResult2, dist2 ) );
-    }
+    @Shadow
+    BlockState getBlockState(BlockPos pos);
 
     @Shadow
-    @NotNull
-    BlockState getBlockState(BlockPos pos);
+    FluidState getFluidState(BlockPos pos);
+
+    @Shadow
+    @Nullable
+    BlockHitResult clipWithInteractionOverride(Vec3 startVec, Vec3 endVec, BlockPos pos, VoxelShape shape, BlockState state);
 }
