2931988f3fdc8f57322bad0ea8734332f1cc9d8c
Ada Aster <an.ada.poirier@gmail.com>
2026-08-24 08:52:12 -0400

    Reorganize and add abstract dripstone impl

--- common/src/main/java/net/countered/terrainslabs/api/helperInterface/IAttachedFaceOffset.java
+++ common/src/main/java/net/countered/terrainslabs/api/helperInterface/IAttachedFaceOffset.java
@@ -1,5 +1,6 @@
-package net.countered.terrainslabs.api;
+package net.countered.terrainslabs.api.helperInterface;
 
+import net.countered.terrainslabs.api.IConditionalOffset;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockGetter;
